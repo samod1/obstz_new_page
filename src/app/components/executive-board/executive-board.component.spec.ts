2ab37afb6b9b47f922a2fa9efff28f57e34c1965
Samuel Domin <samuel.domin@dominanz.sk>
2023-05-25 11:54:04 +0200

    creating component of exe-board

---
+++ src/app/components/executive-board/executive-board.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExecutiveBoardComponent } from './executive-board.component';
+
+describe('ExecutiveBoardComponent', () => {
+  let component: ExecutiveBoardComponent;
+  let fixture: ComponentFixture<ExecutiveBoardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ExecutiveBoardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ExecutiveBoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/executive-board/executive-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-executive-board',
+  templateUrl: './executive-board.component.html',
+  styleUrls: ['./executive-board.component.scss']
+})
+export class ExecutiveBoardComponent {
+
+}
