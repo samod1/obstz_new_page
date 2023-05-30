c66c566d200a4587ca7b5052ed5eb81923eb696a
Samuel Domin <samuel.domin@dominanz.sk>
2023-05-30 16:51:33 +0200

    adding of components to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
+import { ExecutiveBoardComponent } from './components/executive-board/executive-board.component';
+import { VarSymbolComponent } from './components/var-symbol/var-symbol.component';
+import { ExecutiveMinutesComponent } from './components/executive-minutes/executive-minutes.component';
+import { ExecutiveComiteesComponent } from './components/executive-comitees/executive-comitees.component';
+import { RegionalConferenceComponent } from './components/regional-conference/regional-conference.component';
 
 const routes: Routes = [
   {
@@ -8,6 +14,31 @@ const routes: Routes = [
     component: HomeComponent,
     pathMatch: 'full'
   },
+  {
+    path:'executive-board',
+    component: ExecutiveBoardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'executive-minutes',
+    component: ExecutiveMinutesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'variabile-symbol',
+    component: VarSymbolComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'executive-comitees',
+    component: ExecutiveComiteesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'conference-minutes',
+    component: RegionalConferenceComponent,
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
