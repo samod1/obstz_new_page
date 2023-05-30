445ff4fc0eff4a7fb4d9004186f5d590874dee59
Samuel Domin <samuel.domin@dominanz.sk>
2023-05-30 16:49:13 +0200

    changes in enum

--- src/app/enums/enumerations.ts
+++ src/app/enums/enumerations.ts
@@ -1,12 +1,20 @@
 export class Enumerations{
     public static clubs = [
-        {label: "STK Bojnicky", value: "stk_bojnicky"},
-        {label: "TTC Majcichov", value: "ttc_majcichov"},
-        {label: "SK Blava Jaslovske Bohunice ", value: "Sk_blava_jaslovske_bohunice"}
+        {label: "STK Bojnicky", value: "12"},
+        {label: "TTC Majcichov", value: "11"},
+        {label: "SK Blava Jaslovske Bohunice ", value: "13"}
+    ];
+
+    public static season = [
+
+        {label: "2023/2024", value: "2324"},
+        {label: "2024/2025", value: "2425"},
+        {label: "2023/2024", value: "2526"}
+
     ];
 
     public static payment = [
-        {label: "Startovne", value:"fee"},
-        {label: "Pokuta", value:"fine"}
+        {label: "Startovne", value:"01"},
+        {label: "Pokuta", value:"02"}
     ];
 }
